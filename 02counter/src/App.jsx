8ff4e07f72abb4eb377d94f645861fb9ee399aeb
YASH-SAWARKAR <nitintaronejnt@gmail.com>
2025-06-17 23:13:33 +0530

    Initial commit with full project and my changes

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,23 +1,28 @@
-import { userState, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
   const [cnt, setCnt] = useState(0);
 
   const addValue = () => {
-    setCnt(cnt + 1);
+    if (cnt < 20) {
+      setCnt(cnt + 1);
+    }
   };
 
   const removeValue = () => {
-    setCnt(cnt - 1);
+    if (cnt > 0) {
+      setCnt(cnt - 1);
+    }
   };
+
   return (
     <>
       <h1>Chai aur React</h1>
       <h2>Counter Value : {cnt}</h2>
       <button onClick={addValue}>Add Value</button>
       <br />
-      <button onClick={removeValue}>Remover Value</button>
+      <button onClick={removeValue}>Remove Value</button>
     </>
   );
 }
