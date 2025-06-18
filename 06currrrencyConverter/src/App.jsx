688601741edff8029e0c449afe0be81827a888bc
YASH-SAWARKAR <nitintaronejnt@gmail.com>
2025-06-18 18:00:31 +0530

    initiate Currency Converter app

---
+++ 06currrrencyConverter/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-white font-bold bg-orange-400">Currency App</h1>
+    </>
+  );
+}
+
+export default App;
